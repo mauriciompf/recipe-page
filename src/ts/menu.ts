36d6428c419525e5c82275e3289c06da3f558d70
mpfarias <fariasmauricio11@outlook.com>
2024-03-05 10:19:43 -0300

    Update file

--- src/ts/menu.ts
+++ src/ts/menu.ts
@@ -2,30 +2,52 @@ import { getElement } from "./getElement";
 
 export const btnOpenMenu = getElement<HTMLButtonElement>(".btn-open-menu");
 const menuBox = getElement<HTMLDivElement>(".menu");
+
+const boxes = document.querySelectorAll<HTMLDivElement>(".boxes");
+
 const btnOpenPalete = getElement<HTMLButtonElement>(".btn-open-palete");
 const paleteBox = getElement<HTMLDivElement>(".palete");
+
+const fontSizeBox = getElement<HTMLDivElement>(".font-size");
+const btnOpenFontSize = getElement<HTMLButtonElement>(".btn-open-font-size");
+
 const settingButtons =
   document.querySelectorAll<HTMLButtonElement>(".btn-setting");
-const btnCloseMenu = getElement<HTMLButtonElement>(".btn-close-menu");
+const btnCloseBoxes =
+  document.querySelectorAll<HTMLButtonElement>(".btn-close-menu");
 
 btnOpenMenu.addEventListener("click", () => {
   menuBox.classList.toggle("hidden");
+});
+
+btnCloseBoxes.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    toggleSettingsButtons();
+
+    boxes.forEach((box) => {
+      box.classList.add("hidden");
+    });
+
+    menuBox.classList.remove("left-8");
+  });
 });
 
 btnOpenPalete.addEventListener("click", () => {
   paleteBox.classList.toggle("hidden");
   menuBox.classList.add("left-8");
 
-  settingButtons.forEach((btn) => {
-    btn.classList.add("hidden");
-  });
+  toggleSettingsButtons();
 });
 
-btnCloseMenu.addEventListener("click", () => {
-  settingButtons.forEach((btn) => {
-    btn.classList.remove("hidden");
-  });
+btnOpenFontSize.addEventListener("click", () => {
+  fontSizeBox.classList.toggle("hidden");
+  menuBox.classList.add("left-8");
 
-  paleteBox.classList.toggle("hidden");
-  menuBox.classList.remove("left-8");
+  toggleSettingsButtons();
 });
+
+const toggleSettingsButtons = () => {
+  settingButtons.forEach((btn) => {
+    btn.classList.toggle("hidden");
+  });
+};
