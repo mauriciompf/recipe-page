f6153ca913e9639085becd976a9c969e9255ab23
mpfarias <fariasmauricio11@outlook.com>
2024-03-14 10:37:52 -0300

    init files

--- src/ts/fontSize.ts
+++ src/ts/fontSize.ts
@@ -14,71 +14,76 @@ const fontSizeClasses: string[] = [
   "text-xl",
 ];
 
-let i = 2;
+let currretFont = 2;
 
 const h1 = document.querySelectorAll("h1");
 const h2 = document.querySelectorAll("h2");
 const coTitle = document.querySelectorAll(".co-title");
 
-const titles = [...h1, ...h2, ...coTitle];
-
 function increaseFont() {
-  i++;
+  currretFont++;
 
   applyFontSize();
-  if (i >= 2) {
+}
+
+function decreaseFont() {
+  if (currretFont > 0) {
+    currretFont--;
+    applyFontSize();
+  }
+}
+
+function resetFont() {
+  currretFont = 2;
+  applyFontSize();
+}
+
+function applyFontSize() {
+  if (currretFont < fontSizeClasses.length) {
+    body.className = fontSizeClasses[currretFont];
+  } else {
+    currretFont = fontSizeClasses.length - 1;
+  }
+
+  if (currretFont >= 2) {
     h1.forEach((title) => {
-      title.classList.remove(fontSizeClasses[3]);
+      title.classList.remove(...fontSizeClasses);
       title.classList.add("text-3xl");
     });
 
     h2.forEach((title) => {
-      title.classList.remove(fontSizeClasses[2]);
-      title.classList.add("text-3xl");
+      title.classList.remove(...fontSizeClasses);
+      title.classList.add("text-2xl");
     });
 
     coTitle.forEach((title) => {
-      title.classList.remove(fontSizeClasses[1]);
-      title.classList.add("text-2xl");
+      title.classList.remove(...fontSizeClasses);
+      title.classList.add("text-xl");
     });
   }
-}
 
-function decreaseFont() {
-  if (i > 0) {
-    i--;
-    applyFontSize();
+  if (currretFont < 2) {
+    h1.forEach((title) => {
+      title.classList.remove("text-3xl");
+      title.classList.add(fontSizeClasses[4]);
+    });
 
-    if (i < 2) {
-      h1.forEach((title) => {
-        title.classList.remove("text-3xl");
-        title.classList.add(fontSizeClasses[4]);
-      });
+    h2.forEach((title) => {
+      title.classList.remove("text-2xl");
+      title.classList.add(fontSizeClasses[3]);
+    });
 
-      h2.forEach((title) => {
-        title.classList.remove("text-2xl");
-        title.classList.add(fontSizeClasses[3]);
-      });
-
-      coTitle.forEach((title) => {
-        title.classList.remove("text-xl");
-        title.classList.add(fontSizeClasses[3]);
-      });
-    }
-  }
-}
-
-function applyFontSize() {
-  if (i < fontSizeClasses.length) {
-    body.className = fontSizeClasses[i];
-  } else {
-    i = fontSizeClasses.length - 1;
+    coTitle.forEach((title) => {
+      title.classList.remove("text-xl");
+      title.classList.add(fontSizeClasses[2]);
+    });
   }
 }
 
 export default () => {
-  body.className = fontSizeClasses[2];
+  // body.className = fontSizeClasses[2];
 
   btnIncrease.addEventListener("click", increaseFont);
   btnDecrease.addEventListener("click", decreaseFont);
+  btnReset.addEventListener("click", resetFont);
 };
